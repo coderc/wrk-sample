a18386e46fa4b91e2cf3cb71dece78b58da75d98
shaochong <shaochong@inplayable.com>
2023-10-27 11:11:06 +0800

    [update]

--- test_server/main.go
+++ test_server/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	r := gin.New()
-	r.POST("/dsp/dsp_dispatcher", func(c *gin.Context) {
+	r.Match([]string{"POST", "GET"}, "/dsp/dsp_dispatcher", func(c *gin.Context) {
 		body, _ := io.ReadAll(c.Request.Body)
 
 		fmt.Printf("param: %s\nexchange: %s\nbody: %s\n", c.Request.URL.RawQuery, c.Query("exchange"), string(body))
