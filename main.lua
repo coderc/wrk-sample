7b9c013214e640371df8c6ebd55f6800eee44fab
shaochong <shaochong@inplayable.com>
2023-10-27 11:04:54 +0800

    [update]

--- main.lua
+++ main.lua
@@ -1,32 +1,34 @@
--- example HTTP POST script which demonstrates setting the
--- HTTP method, body, and adding a header
+local level = 0
+
+
+function consoleErr(s) if level > 1 then return end consoleLog(string.format("%s %s", "[ERROR]", s)) end
+function consoleWarn(s) if level > 1 then return end consoleLog(string.format("%s %s", "[WARN]", s))  end
+function consoleInfo(s) if level > 0 then return end consoleLog(string.format("%s %s", "[INFO]", s))  end
+function consoleDebug(s) if level > -1 then return end consoleLog(string.format("%s %s", "[DEBUG]", s))  end
+function consoleLog(s)
+    print(s)
+end
 
 -- ⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️必须检查⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
 -- 指定运行路径
 local runPath = "/Users/shaochong/iplayable/wrk/"
 -- 指定从哪个文件中读取request数据
-local file = "sample.request"
+local file = "10000-data-webeye-vlionmobi.request"
+local delayNumber = 10 -- 单位: ms
+local splitChar = ","
 -- ⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️必须检查⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️
 
 -- 开启debug会显示每次请求的exchange和body
-local debug = true
-
 fileOpen = io.open(runPath .. file, "r")
 io.input(fileOpen)
--- local body = io.read("*a")
--- io.close(fileOpen)
 
--- 设置target server的ip和端口
-wrk.scheme = "http"
-wrk.host = "127.0.0.1"
-wrk.port = 8088
-path = "/dsp/dsp_dispatcher"
+route = "/dsp/dsp_dispatcher"
 wrk.method = "POST"
 wrk.body   = io.read()
 wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
 
 findIdx = function(str, ch)
-    idx = 0
+    idx = -1
     for i = 0, string.len(str) do
         if string.sub(str, i, i) == ch
         then
@@ -38,6 +40,7 @@ findIdx = function(str, ch)
 end
 
 -- read single line from file
+-- in: string, out: map{[body: post request body], [other: url params]}
 readLine = function()
     line = io.read()
     if line == nil
@@ -48,39 +51,62 @@ readLine = function()
         line = io.read()
     end
 
-    idx = findIdx(line, ",")
-    exchange = string.sub(line, 1, idx - 1)
-    body = string.sub(line, idx + 2, string.len(line) - 1)
-
-    if debug
+    kvs = {}
+    idx = findIdx(line, splitChar)
+    if idx == -1
     then
-        print("exchange: [" .. exchange .. "]")
-        print("body: [" .. body .. "]")
+        consoleErr(string.format("read line failed, line: [%s], splitChar: [%s]", line, splitChar))
+        return nil, nil
     end
-    return exchange, body
+    kvs.exchange = string.sub(line, 1, idx - 1)
+    kvs.body = string.sub(line, idx + 2, string.len(line) - 1)
+    consoleDebug(map2str(kvs))
+    return kvs
+end
+
+function map2str(kvs)
+    s = ""
+    for k, v in pairs(kvs) do
+        s = s .. k .. "=" .. v
+    end
+    return s
+end
+
+
+function map2paramStr(kvs)
+    s = ""
+    for k, v in pairs(kvs) do
+        if k ~= "body"
+        then
+            s = s .. k .. "=" .. v .. "&"
+        end
+    end
+    return s
 end
 
 -- delay 设置每个请求的间隔时间(单位:毫秒)
 delay = function()
-    return 2000
+    return delayNumber
 end
 
 request = function()
     -- 设置请求体
-    exchange, body = readLine()
+    kvs = readLine()
 
-    if exchange == nil or body == nil
+    if kvs.exchange == nil or kvs.body == nil
     then
-        print("exchange or body is nil")
+        consoleErr("exchange or body is nil")
         return
     end
-    wrk.path = path .. "?exchange=" .. exchange
-    wrk.body   = body
+    wrk.path = route .. "?" .. map2paramStr(kvs)
+    wrk.body = kvs.body
+    consoleDebug(map2str(kvs))
     return wrk.format()
 end
 
 -- 单线程单连接的测试shell
--- ./wrk/wrk -d4s -c1 -t1 --latency -s main.lua http://0.0.0.0:8088
+-- ./wrk/wrk -d3s -c1 -t1 --latency -s main.lua http://127.0.0.1:8088
+-- ./wrk -d4s -c1 -t1 --latency -s main.luatest.tualta.com
 -- 双线程双连接的测试shell
 -- ./wrk/wrk -d10s -c2 -t2 --latency -s main.lua http://0.0.0.0:8088
 
@@ -91,4 +117,4 @@ end
 
 
 -- ./wrk/wrk -d1m -c1 -t1 -s main.lua http://192.168.3.57:8348
--- ./wrk/wrk -d1m -c4 -t4 -s main.lua http://192.168.3.57:8348
+-- ./wrk/wrk -d1m -c4 -t4 -s main.lua http://13.212.91.81:8008
